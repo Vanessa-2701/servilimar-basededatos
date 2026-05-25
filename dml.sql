643f322db280a3eb742a7399c658e636452f870d
Vanessa-2701 <leidy.ruano@correounivalle.edu.co>
2026-05-25 15:29:34 -0500

    Create dml.sql

---
+++ dml.sql
@@ -0,0 +1,9 @@
+INSERT INTO Ciudad(nombre_ciudad, codigo_postal) VALUES
+('Cali','760001'),
+('Bogotá','110111');
+
+INSERT INTO Servicio(nombre, descripcion, estado) VALUES
+('Atención al cliente','Soporte general','Activo');
+
+INSERT INTO Medio(nombre_medio) VALUES
+('Correo');
